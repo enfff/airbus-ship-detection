9baa3ee65bbc2f1ceabf4e70ea18254815f4d81f
enfff <enforcetitan@gmail.com>
2024-05-24 16:32:41 +0200

    aggiunto tributo amichetti

--- cursor_automove.py
+++ cursor_automove.py
@@ -1,15 +1,29 @@
 import pyautogui
 import time
+import random
+
+simo = ["bello", "intelligente", "attraente", "buono"]
+enf = ["bello", "intelligente", "attraente", "buono"]
+ludo = ["bella", "intelligente", "attraente", "buona"]
+
+arrays = {
+    "simo": simo,
+    "enf": enf,
+    "ludo": ludo,
+}
+
 
 while True:
+    time.sleep(5)
+
     pyautogui.moveTo(700, 300)
     pyautogui.click()
-    pyautogui.write('Hello, world!')
-    
-    time.sleep(30*60)
 
+    selected_name, selected_array = random.choice(list(arrays.items()))
+    pyautogui.write(f"{selected_name} e' {random.choice(selected_array)}")
+    
+    time.sleep(5)
     pyautogui.moveTo(650, 350)
     pyautogui.click()
-    pyautogui.write('How are you?')
-    
-    time.sleep(30*60)
+    selected_name, selected_array = random.choice(list(arrays.items()))
+    pyautogui.write(f"{selected_name} e' {random.choice(selected_array)}")
--- elapsed_time.py
+++ elapsed_time.py
@@ -17,6 +17,6 @@ def calculate_elapsed_time(file_path):
         
         return elapsed_time
 
-file_path = "models/log_05-20_13.54.42.txt"  # Replace with the path to your file
+file_path = "models/model_epochs20_noaug_id3/log_05-21_07.38.50.txt"  # Replace with the path to your file
 elapsed_time = calculate_elapsed_time(file_path)
 print("Total elapsed time: ", elapsed_time)
