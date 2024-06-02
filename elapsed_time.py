ac4a9df47bd3b4bcadd00403f4caa6590b7d96f0
enfff <enforcetitan@gmail.com>
2024-06-02 12:09:39 +0200

    ocd at its finest

--- elapsed_time.py
+++ elapsed_time.py
@@ -10,18 +10,9 @@ with open(file_path, 'r') as file:
     lines = [line for line in lines_buf if pattern.search(line)]
 
 def calculate_elapsed_time(lines=lines):
-
     start_time = datetime.strptime(lines[0].split(' ')[0] + ' ' + lines[0].split(' ')[1], '%m-%d %H:%M:%S')
     end_time = datetime.strptime(lines[-1].split(' ')[0] + ' ' + lines[-1].split(' ')[1], '%m-%d %H:%M:%S')
-
     return end_time - start_time
 
-def mean_train_time(lines=lines):
-
-    start_time = datetime.strptime(lines[0].split(' ')[0] + ' ' + lines[0].split(' ')[1], '%m-%d %H:%M:%S')
-    end_time = datetime.strptime(lines[-1].split(' ')[0] + ' ' + lines[-1].split(' ')[1], '%m-%d %H:%M:%S')
-    
-    return (end_time - start_time) / len(lines)
-
-print("(TRAIN) Total elapsed time: ", calculate_elapsed_time(), "(hours)")
-print("(TRAIN) Mean batch training time: ", mean_train_time().total_seconds(), "(secs)")
+print(f"(TRAIN) Total elapsed time: {calculate_elapsed_time()} (hours)")
+print(f"(TRAIN) Mean batch training time: {float(calculate_elapsed_time().total_seconds()/len(lines)).__round__(4)} (secs)")
